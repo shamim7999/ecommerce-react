22255d1fe8e7159ea1039321e2461b413b7cb043
shamim7999 <sarker.shamim1998@gmail.com>
2024-03-27 06:32:12 +0600

    sk;fjajas

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import { FaShoppingBag } from "react-icons/fa";
+
+
+import '../css/Products.css'
 
 const Card = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='card'>
+            <img src='https://m.media-amazon.com/images/I/51+P9uAvb1L._AC_UY695_.jpg'
+                alt='No Images' className='card-img' />
+            <div className='card-details'>
+                <h3 className='card-title'>Shoe</h3>
+                <section className='card-reviews'>
+                    <AiFillStar className='ratings-start' /> <AiFillStar className='ratings-start' /> 
+                    <AiFillStar className='ratings-start' /> <AiFillStar className='ratings-start' />
+                    <span className='total-reviews'>4</span>
+                </section>
+                <section className='card-price'>
+                    <div className='price'>
+                        <del>$300</del> $200
+                    </div>
+                    <div className='bag'>
+                        <FaShoppingBag className='bag-icon' />
+                    </div>
+                </section>
+            </div>
+        </section>
   )
 }
 
--- src/sidebar/color/Colors.js
+++ src/sidebar/color/Colors.js
@@ -5,9 +5,35 @@ import '../../css/Colors.css'
 const Colors = () => {
   return (
     <div>
-      Colors
-    </div>
+      <label className='sidebar-label-container color-title'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>All
+      </label>
+      <label className='sidebar-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>Black
+      </label>
+      <label className='sidebar-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>Blue
+      </label>
+      <label className='sidebar-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>Red
+      </label>
+      <label className='sidebar-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>Green
+      </label>
+      <label className='sidebar-label-container'>
+        <input type='radio' name='test2' />
+        <span className='checkmark'></span>White
+      </label>
+
+
+      </div>
   )
 }
 
 export default Colors
+ 
