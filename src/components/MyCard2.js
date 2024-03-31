c095a0045d2f31e372e74f6fef5ef91447a445df
shamim7999 <10shamimsarker01@gmail.com>
2024-03-31 15:30:26 +0600

    Toast added

--- src/components/MyCard2.js
+++ src/components/MyCard2.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import { Button, Card, CardGroup, Col} from 'react-bootstrap';
+import { Button as MyButton ,Card, CardGroup, Col} from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 import '../css/MyImage.css'
 
 const MyCard2 = ({id, img, title, star, reviews, prevPrice, newPrice, amount, handleClickOnCart, category}) => {
     const item = {id, img, title, star, reviews, prevPrice, newPrice, amount, category};  
     
-    const handleClick = (e) => {
+    
+  const [snackPack, setSnackPack] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState(undefined);
+
+  React.useEffect(() => {
+    if (snackPack.length && !messageInfo) {
+      // Set a new snack when we don't have an active one
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpen(true);
+    } else if (snackPack.length && messageInfo && open) {
+      // Close an active snack when a new one is added
+      setOpen(false);
+    }
+  }, [snackPack, messageInfo, open]);
+
+  const handleClick = (e) => {
+      setSnackPack((prev) => [...prev, { e, key: new Date().getTime() }]);
       console.log(`title: ${title} and id: ${item.amount}`)
       handleClickOnCart(item, 1);
-      alert(`${title} added to cart`);
-    } 
-    
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleExited = () => {
+    setMessageInfo(undefined);
+  };
+
+
     return (
     <>
       <Col md={6} lg={3}>
@@ -28,13 +62,37 @@ const MyCard2 = ({id, img, title, star, reviews, prevPrice, newPrice, amount, ha
               <h5><del>${prevPrice}</del> &nbsp; &nbsp; ${newPrice} </h5>
             </Card.Body>
             <Card.Footer>
-            <Button onClick={handleClick} variant="success">Add to Cart</Button>{' '}
+            <MyButton onClick={handleClick} variant="success">Add to Cart</MyButton>{' '}
             </Card.Footer>
           </Card>
         </CardGroup>
+
+        
       </Col>
 
-      
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        TransitionProps={{ onExited: handleExited }}
+        message={messageInfo ? messageInfo.message : undefined}
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              <div style={{color: 'white'}}>Added To cart</div>
+            </Button>
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              sx={{ p: 0.5 }}
+              onClick={handleClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
 
     </>
   );
--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,28 +1,66 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Toast from 'react-bootstrap/Toast';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
-function MyToast() {
-  const [showA, setShowA] = useState(true);
-  const [showB, setShowB] = useState(true);
+export default function ConsecutiveSnackbars() {
+  const [snackPack, setSnackPack] = React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState(undefined);
 
-  const toggleShowA = () => setShowA(!showA);
-  const toggleShowB = () => setShowB(!showB);
+  React.useEffect(() => {
+    if (snackPack.length && !messageInfo) {
+      // Set a new snack when we don't have an active one
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpen(true);
+    } else if (snackPack.length && messageInfo && open) {
+      // Close an active snack when a new one is added
+      setOpen(false);
+    }
+  }, [snackPack, messageInfo, open]);
+
+  const handleClick = (message) => () => {
+    setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleExited = () => {
+    setMessageInfo(undefined);
+  };
 
   return (
     <>
-    <Toast>
-    <Toast.Header>
-      <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-      <strong className="me-auto">Bootstrap</strong>
-      <small>11 mins ago</small>
-    </Toast.Header>
-    <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
-  </Toast>
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        TransitionProps={{ onExited: handleExited }}
+        message={messageInfo ? messageInfo.message : undefined}
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              <div style={{color: 'white'}}>Added To cart</div>
+            </Button>
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              sx={{ p: 0.5 }}
+              onClick={handleClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
     </>
   );
 }
-
-export default MyToast;
